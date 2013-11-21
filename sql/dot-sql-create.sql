71bb923e382a03ac7db06c7c3d025206aaccfcbd
David Martel <008544794@ecs407mac198.cecs.csulb.edu>
2013-11-21 12:10:25 -0800

    Fixed some typeos with enums

--- sql/dot-sql-create.sql
+++ sql/dot-sql-create.sql
@@ -7,62 +7,65 @@ DROP TABLE contributors;
 DROP TABLE changes;
 DROP TABLE commits;
 DROP TABLE projects;
+DROP TABLE type;
+DROP TABLE status;
+DROP TABLE priority
 
 
 --the following three tables create our enumerated data types
 CREATE TABLE type(
 	name ENUM('BUG', 'IMPROVEMENT', 'LONG-TERM')
 );
-CREATE TABLE status{
+CREATE TABLE status(
 	name ENUM('OPEN', 'CLOSED', 'NOFIX', 'ASSIGNED', 'DUPLICATE', 'POSTPONED')
-}
-CREATE TABLE priority{
+);
+CREATE TABLE priority(
 	name ENUM('CRITICAL', 'MEDIUM', 'LOW')
-} 
+);
 --the following 9 tables are for our data
 CREATE TABLE projects(
-	title VARCHAR(20) (NOT NULL),
+	title VARCHAR(20) NOT NULL,
 	dateToEnd TIMESTAMP,
 	description TEXT,
-	dateStarted DATE (NOT NULL),
-	id INT (NOT NULL) AUTO_INCREMENT,
+	dateStarted DATE NOT NULL,
+	id INT NOT NULL AUTO_INCREMENT,
 	CONSTRAINT projects_pk PRIMARY KEY (id),
 	CONSTRAINT projects_ck1 UNIQUE (title, startDate),
 	CONSTRAINT valid_data_range CHECK (dateToEnd >= dateStarted)
 );
 CREATE TABLE contributors(
-	id INT (NOT NULL) AUTO_INCREMENT,
+	id INT NOT NULL AUTO_INCREMENT,
 	fName VARCHAR(15),
 	lName VARCHAR(15),
-	email VARCHAR(30) (NOT NULL),
+	email VARCHAR(30) NOT NULL,
 	CONSTRAINT contributors_pk PRIMARY KEY(id),
 	CONSTRAINT contributors_ck UNIQUE (email)
 );
 CREATE TABLE managementAssignments(
-	dateStarted TIMESTAMP (NOT NULL),
+	dateStarted TIMESTAMP NOT NULL,
 	dateToEnd TIMESTAMP,
 	finished BOOLEAN,
-	projectID INT (NOT NULL),
-	contributorID INT (NOT NULL) ,
+	projectID INT NOT NULL,
+	contributorID INT NOT NULL ,
 	CONSTRAINT managementAssignments_pk PRIMARY KEY (projectID, contributorID, dateStarted),
 	CONSTRAINT managementAssignments_fk FOREIGN KEY (projectID) REFERENCES projects (id) ON DELETE CASCADE,
 	CONSTRAINT managementAssignments_fk2 FOREIGN KEY (contributorID) REFERENCES contributors (id) ON DELETE CASCADE
 );
 CREATE TABLE phoneNumbers(
-	contributorID INT (NOT NULL),
-	phoneNumber VARCHAR(11) (NOT NULL),
+	contributorID INT NOT NULL,
+	phoneNumber VARCHAR(11) NOT NULL,
 	phoneType VARCHAR(10)
 	CONSTRAINT phoneNumbers_pk PRIMARY KEY (contributorID, phoneNumber, phoneType),
 	CONSTRAINT phoneNumbers_fk FOREIGN KEY (contributorID) REFERENCES contributors (id) ON DELETE CASCADE
 );
 CREATE TABLE goals(
-	id INT (NOT NULL) AUTO_INCREMENT,
-	title VARCHAR(20) (NOT NULL),
-	description TEXT (NOT NULL),
+	id INT NOT NULL AUTO_INCREMENT,
+	title VARCHAR(20) NOT NULL,
+	description TEXT NOT NULL,
 	priority PRIORITY,
 	type TYPE,
 	status STATUS,
-	dateCreated TIMESTAMP(NOT NULL),
+	dateCreated TIMESTAMPNOT NULL,
 	dateUpdated TIMESTAMP,
 	dateToEnd TIMESTAMP,
 	projectID INT,
@@ -73,18 +76,18 @@ CREATE TABLE goals(
 );
 CREATE TABLE posts (
 	body TEXT,
-	dateAndTime TIMESTAMP(NOT NULL),
-	contributorID INT (NOT NULL), --need to change the relational scheme
+	dateAndTime TIMESTAMPNOT NULL,
+	contributorID INT NOT NULL, --need to change the relational scheme
 	goalID INT,
 	CONSTRAINT posts_pk PRIMARY KEY (datePosted, time, goalID),
 	CONSTRAINT posts_fk FOREIGN KEY (goalID) REFERENCES goals(id) ON DELETE CASCADE
 );
 CREATE TABLE workAssignments(
-	dateStarted TIMESTAMP (NOT NULL),
+	dateStarted TIMESTAMP NOT NULL,
 	dateToEnd TIMESTAMP,
 	finished BOOLEAN,
-	goalID (NOT NULL),
-	contributorID (NOT NULL),
+	goalID NOT NULL,
+	contributorID NOT NULL,
 	CONSTRAINT workAssignments PRIMARY KEY(dateStarted, goalID, contributorID)
 );
 CREATE TABLE commits(
@@ -92,17 +95,17 @@ CREATE TABLE commits(
 	goalID INT, 	--should this be not null? 
 	commitDate TIMESTAMP,
 	description TEXT,
-	id INT (NOT NULL) AUTO_INCREMENT, 
+	id INT NOT NULL AUTO_INCREMENT, 
 	CONSTRAINT changes_pk PRIMARY KEY(id),
 	CONSTRAINT changes_fk FOREIGN KEY(contributorID) REFERENCES contributors(id) ON DELETE CASCADE,
 	CONSTRAINT changes_fk FOREIGN KEY(goalID) REFERENCES goalss(id) ON DELETE CASCADE, 
 	CONSTRAINT changes_ck UNIQUE (commitDate, time, description)
 );
 CREATE TABLE changes(
-	fileAdjusted VARCHAR(20) (NOT NULL),
+	fileAdjusted VARCHAR(20) NOT NULL,
 	bodyOfDiff TEXT,
 	checkSum CHAR(10),
-	commitID INT (NOT NULL),
+	commitID INT NOT NULL,
 	CONSTRAINT changes_pk PRIMARY KEY(commitID, fileAdjusted), 
 	CONSTRAINT changes_fk FOREIGN KEY(commitID) REFERENCES commits(id) ON DELETE CASCADE
 );
