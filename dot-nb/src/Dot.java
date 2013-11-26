578ded1f12d3e8966cc25f058ae42cd7be8105b2
Tan Tran <tankimtran@gmail.com>
2013-11-26 10:47:57 -0800

    println statements for main menu

--- dot-nb/src/Dot.java
+++ dot-nb/src/Dot.java
@@ -1,11 +1,22 @@
 public class Dot {
-	static User guy;
-	static String workingDirectory
+	//static User guy;
+	//static String workingDirectory
 
-	public static void main(String args[]) {
-		//load global config file
-
-		//handle arguments
-		
+	public static void main(String args[]) 
+        {
+            System.out.println("Welcome to the management console of the DOT issue tracker and source control program.");
+            System.out.println("This is the main menu of what you can do on your project.");
+            
+            System.out.println("1. Manage COMMITS of source code.");
+            System.out.println("2. Manage POSTS on issues.");
+            System.out.println("3. Manage GOALS.");
+            System.out.println("4. Manage CONTRIBUTORS.");
+            System.out.println("5. COMMIT changes to the database.");
+            System.out.println("6. ROLLBACK changes since the last commit.");
+            System.out.println("7. QUIT.");
+            
+            
 	}
+        
+        
 }
