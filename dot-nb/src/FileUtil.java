d88fe31a3528d95594a933314a107af0ec3a444b
David Martel <DavidKMartel@gmail.com>
2013-12-03 11:31:49 -0800

    Added FileUtil for all file manipulation functions

---
+++ dot-nb/src/FileUtil.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author david
+ * This class is for organizing all file functions. Silly java for forcing 
+ * objects everywhere.
+ */
+public class FileUtil {
+    
+    public static String diff(String oldFile, String newFile) {
+        return null;
+    }
+    
+}
