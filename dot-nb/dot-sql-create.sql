aa2ffe223d790a671033c15a58ac0ea09df3e3fc
David Gaskins <david.s.gaskins@gmail.com>
2013-12-05 11:22:15 -0800

    checking to see if comments are ruining the sql insert

--- dot-nb/dot-sql-create.sql
+++ dot-nb/dot-sql-create.sql
@@ -35,13 +35,12 @@ CREATE TABLE contributors(
 	fName VARCHAR(15),
 	lName VARCHAR(15),
 	email VARCHAR(30) NOT NULL, 
+
+	CONSTRAINT contributors_pk PRIMARY KEY(id),
 	--  Contributors must be able to be contacted thru email
-	
 	--  Commit, Post, ManagementAssignment, and WorkAssignment
 	--  all reference Contributor. Use surrogate key "id" to 
 	--  reduce memory usage, and becaue CK email may change
-	CONSTRAINT contributors_pk PRIMARY KEY(id),
-	
 	--  Assume no Contributors share email
 	CONSTRAINT contributors_ck UNIQUE (email)
 );
